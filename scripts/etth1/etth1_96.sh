68e2b0499dab721e26424f4a557221182c500e76
TimeVVVMixDFANet <liweihuman@gmail.com>
2026-02-10 11:18:44 +0800

    Update etth1_96.sh

--- scripts/etth1/etth1_96.sh
+++ scripts/etth1/etth1_96.sh
@@ -3,9 +3,14 @@ model_name=FreqMixAttNet
 run_date='test'
 root_path='./data'
 
-seq_len=96
+
+
+
+weight_att=0.01
+sr_ratio = 8
+seq_len=192
 e_layers=2
-learning_rate=0.02
+learning_rate=0.01
 d_model=32
 n_heads=8
 d_ff=32
@@ -15,10 +20,10 @@ batch_size=128
 dropout=0.1
 down_sampling_layers=2
 down_sampling_window=2
-freq_weight=4
+freq_weight=8
 alpha=0.7 
-l1l2_alpha=0.035
-aug_weight=0.017
+l1l2_alpha=0.0
+aug_weight=0.07
 mix_rate=0.1
 jitter_ratio=0.3
 devices='0'
@@ -47,6 +52,8 @@ python -u run_model.py \
 --patch_len 16 \
 --d_model $d_model \
 --n_heads $n_heads \
+--weight_att $weight_att \
+--sr_ratio $sr_ratio \
 --d_ff $d_ff \
 --down_sampling_layers $down_sampling_layers \
 --down_sampling_window $down_sampling_window \
