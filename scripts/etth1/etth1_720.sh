48baae32945f976ed3d969bf6368046a4ec2c85c
TimeVVVMixDFANet <liweihuman@gmail.com>
2026-02-11 14:10:42 +0800

    Update etth1_720.sh

--- scripts/etth1/etth1_720.sh
+++ scripts/etth1/etth1_720.sh
@@ -3,27 +3,33 @@ model_name=FreqMixAttNet
 run_date='test'
 root_path='./data'
 
-seq_len=96
-e_layers=2
+
+pred_len=720
 learning_rate=0.01
-d_model=32
+dropout=0.2
+
+sr_ratio= 8
 n_heads=8
+d_model=16
+freq_weight=8
+weight_att=0.03
+seq_len=192
+
+aug_weight=0.07
+batch_size=128
+alpha=0.7 
+
+e_layers=2
+l1l2_alpha=0.00
 d_ff=32
 train_epochs=6
 patience=6
-batch_size=128
-dropout=0.15
 down_sampling_layers=2
 down_sampling_window=2
-freq_weight=8
-alpha=0.5 
-l1l2_alpha=0.035
-aug_weight=0.02
 mix_rate=0.1
 jitter_ratio=0.3
 devices='0'
 decomp_method='wavelet'
-aug_constrast_weight2=0.01
 
 python -u run_model.py \
 --gpu 0 \
@@ -38,7 +44,7 @@ python -u run_model.py \
 --features M \
 --seq_len $seq_len \
 --label_len 0 \
---pred_len 720 \
+--pred_len $pred_len \
 --e_layers $e_layers \
 --decomp_method wavelet \
 --enc_in 7 \
@@ -48,6 +54,8 @@ python -u run_model.py \
 --patch_len 16 \
 --d_model $d_model \
 --n_heads $n_heads \
+--weight_att $weight_att \
+--sr_ratio $sr_ratio \
 --d_ff $d_ff \
 --down_sampling_layers $down_sampling_layers \
 --down_sampling_window $down_sampling_window \
@@ -62,5 +70,4 @@ python -u run_model.py \
 --batch_size $batch_size \
 --aug_weight $aug_weight \
 --mix_rate $mix_rate \
---jitter_ratio $jitter_ratio \
---aug_constrast_weight2 $aug_constrast_weight2
+--jitter_ratio $jitter_ratio 
