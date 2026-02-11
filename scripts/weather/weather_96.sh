27bd5237b4a7bfc8fe2c5acae1a5dfa7a9838736
TimeVVVMixDFANet <liweihuman@gmail.com>
2026-02-11 15:18:13 +0800

    Update weather_96.sh

--- scripts/weather/weather_96.sh
+++ scripts/weather/weather_96.sh
@@ -1,45 +1,49 @@
+
 # export CUDA_VISIBLE_DEVICES=0
 
 nvidia-smi
 echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
 python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}'); print(f'CUDA device count: {torch.cuda.device_count()}')"
 
-
 model_name=FreqMixAttNet
 run_date='test'
 root_path='./data'
 
-seq_len=96
+
+
+
+weight_att=0.01
+sr_ratio= 8
+seq_len=192
 e_layers=2
 learning_rate=0.01
 d_model=32
-n_heads=4
+n_heads=8
 d_ff=32
-train_epochs=20
+train_epochs=6
 patience=6
 batch_size=128
 dropout=0.1
 down_sampling_layers=2
 down_sampling_window=2
-aug_constrast_weight1=0.01
-aug_constrast_weight2=0.01
 freq_weight=8
 alpha=0.7 
-l1l2_alpha=0.035
-aug_weight=0.05
+l1l2_alpha=0.0
+aug_weight=0.07
 mix_rate=0.1
 jitter_ratio=0.3
 devices='0'
 decomp_method='wavelet'
+
 
 python -u run_model.py \
 --gpu 0 \
 --task_name long_term_forecast \
 --is_training 1 \
 --devices $devices \
---data_path weather.csv \
+--data_path exchange_rate.csv \
 --root_path $root_path \
---model_id $run_date'_weather' \
+--model_id $run_date'_exchange_rate' \
 --model $model_name \
 --data custom \
 --features M \
@@ -48,13 +52,15 @@ python -u run_model.py \
 --pred_len 96 \
 --e_layers $e_layers \
 --decomp_method wavelet \
---enc_in 21 \
---dec_in 21 \
+--enc_in 7 \
+--c_out 7 \
 --des 'Exp' \
 --itr 1 \
 --patch_len 16 \
 --d_model $d_model \
 --n_heads $n_heads \
+--weight_att $weight_att \
+--sr_ratio $sr_ratio \
 --d_ff $d_ff \
 --down_sampling_layers $down_sampling_layers \
 --down_sampling_window $down_sampling_window \
@@ -69,6 +75,4 @@ python -u run_model.py \
 --batch_size $batch_size \
 --aug_weight $aug_weight \
 --mix_rate $mix_rate \
---aug_constrast_weight1 $aug_constrast_weight1 \
---aug_constrast_weight2 $aug_constrast_weight2 \
---jitter_ratio $jitter_ratio \
+--jitter_ratio $jitter_ratio 
